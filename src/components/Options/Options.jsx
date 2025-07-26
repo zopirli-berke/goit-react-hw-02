319e7ecee8c0ee6d7896888f7bfafacfd9b571d8
Berke <zberke97@gmail.com>
2025-07-26 15:55:34 +0300

    Refactor Options component to use a div wrapper and add CSS module for styling

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,15 +1,17 @@
+import css from "./Options.module.css";
+
 export default function Options({
   updateFeedback,
   resetFeedback,
   totalFeedback,
 }) {
   return (
-    <>
+    <div className={css.options}>
       <button onClick={() => updateFeedback("good")}>Good</button>
       <button onClick={() => updateFeedback("neutral")}>Neutral</button>
       <button onClick={() => updateFeedback("bad")}>Bad</button>
 
       {totalFeedback() > 0 && <button onClick={resetFeedback}>Reset</button>}
-    </>
+    </div>
   );
 }
